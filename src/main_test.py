578477451cc042c0a36c3a76a1009635b869140e
Fernando Duarte Cardo <fernandocardo@gmail.com>
2024-04-22 10:27:59 -0300

    refactor: teste de unidade
    Simplificação para deixar o método e a chamada no mesmo arquivo, para ficar melhor para mostrar no artigo

--- src/main_test.py
+++ src/main_test.py
@@ -1,11 +1,11 @@
-import integracaoAWS
+import main
     
 def test_obterDadosBucket():
-    resultado = integracaoAWS.obterDadosBucket('meu-primeiro-bucket', 'resultado.json')
+    resultado = main.obterDadosBucket('meu-primeiro-bucket', 'resultado.json')
     
     assert resultado == {'nome': 'Joao', 'idade': 25}
 
 def test_obterDadosBucket_erro():
-    resultado = integracaoAWS.obterDadosBucket('meu-primeiro-bucket', 'resultado2.json')
+    resultado = main.obterDadosBucket('meu-primeiro-bucket', 'resultado2.json')
     
     assert resultado == 'Erro ao buscar o arquivo: resultado2.json'
